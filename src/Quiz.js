c01ab011f0f73dec3cff2193b93d3d4a4d1a7717
khichinho <hardikkhichi@gmail.com>
2020-05-31 10:40:53 +0530

    module7 (Quiz Reset) completed

--- src/Quiz.js
+++ src/Quiz.js
@@ -9,18 +9,24 @@ class Quiz extends Component {
     super(props);
     this.state = { quiz_position: 1 };
   }
+
   showNextQuestion() {
     this.setState((state) => {
       return { quiz_position: state.quiz_position + 1 };
     });
   }
+
+  handleResetClick() {
+    this.setState({ quiz_position: 1 });
+  }
+
   render() {
     const isQuizEnd =
       this.state.quiz_position - 1 === quizData.quiz_questions.length;
     return (
       <div>
         {isQuizEnd ? (
-          <QuizEnd />
+          <QuizEnd resetClickHandler={this.handleResetClick.bind(this)} />
         ) : (
           <QuizQuestion
             quiz_question={
